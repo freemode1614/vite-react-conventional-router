037735baeaec0d536ab61c893894f8008904993d
freemode <freemode1614@outlook.com>
2024-09-21 15:16:37 +0800

    chore:精简测试用例

--- example/react/src/App.tsx
+++ example/react/src/App.tsx
@@ -1,20 +1,9 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import routes from "virtual:routes"
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1 role='main'>Vite + React</h1>
       <div className="pages">
         <RouterProvider router={createBrowserRouter(routes)} fallbackElement={
