dec8038ab1295a3c165651d60e326542fd4bd387
freemode1614 <150514318+freemode1614@users.noreply.github.com>
2025-05-09 23:23:26 +0800

    Beta release

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,5 @@
-import { default as config } from "@moccona/eslint-config/flat/node";
+import prettier from "@moccona/eslint-config/extra/prettier";
+import ts from "@moccona/eslint-config/extra/typescript";
+import baseConfig from "@moccona/eslint-config/flat";
 
-/**
- */
-export default [
-  ...config,
-  {
-    ignores: ["example/**"],
-  },
-];
+export default [...baseConfig, ...ts, ...prettier];
